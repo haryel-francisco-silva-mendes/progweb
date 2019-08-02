fde0f7715fc5601c83bc8af6a90ed74b62aa24d9
HARYEL FRANCISCO SILVA MENDES <haryelfsm@hotmail.com>
2019-08-02 11:54:10 -0300

    BD_02/08

--- bd_phpoo/functions/Connection.class.php
+++ bd_phpoo/functions/Connection.class.php
@@ -13,5 +13,28 @@
                 $this->selectDatabase();
             }
         }
+
+        //metodo de seleção do database 
+        private function selectDatabase(){
+            $database= mysqli_select_db($this->connection, $this->parameters["database"]);
+            if(!$database){
+                die("Base de dados nao encontrada");
+            }
+            mysqli_query($this->connection, "SET NAMES 'utf8'");
+            mysqli_query($this->connection, 'SET character_set_connection=utf8');
+            mysqli_query($this->connection, 'SET character_set_client=utf8');
+            mysqli_query($this->connection, 'SET charset_set_results=utf8');
+        }
+
+        //metodo de retorno de conexão
+        public function getConnection(){
+            return $this->connection;
+        }
+
+        //metodo de fechamento da conexao
+
+        public function closeConnection(){
+            mysqli_close($this->connection);
+        }
     }
 ?>
